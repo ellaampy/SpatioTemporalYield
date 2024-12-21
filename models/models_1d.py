1d239c23204bb74b378702f86b3d8801a1dd89b0
A Stella <ellaampy@gmail.com>
2024-12-21 14:02:11 +0100

    remove unused models_1d.py

--- models/models_1d.py
+++ models/models_1d.py
@@ -282,60 +282,7 @@ class FC_BatchNorm_Relu_Dropout(torch.nn.Module):
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
-    
-
-
-
-# https://github.com/MarcCoru/BreizhCrops/blob/master/breizhcrops/models/TransformerModel.py
-class TransformerModel(nn.Module):
-    def __init__(self, input_dim=13, num_classes=9, d_model=64, n_head=2, n_layers=5,
-                 d_inner=128, activation="relu", dropout=0.017998950510888446):
-
-        super(TransformerModel, self).__init__()
-        self.modelname = f"TransformerEncoder_input-dim={input_dim}_num-classes={num_classes}_" \
-                         f"d-model={d_model}_d-inner={d_inner}_n-layers={n_layers}_n-head={n_head}_" \
-                         f"dropout={dropout}"
-
-        encoder_layer = TransformerEncoderLayer(d_model, n_head, d_inner, dropout, activation)
-        encoder_norm = LayerNorm(d_model)
-
-        self.inlinear = Linear(input_dim, d_model)
-        self.relu = ReLU()
-        self.transformerencoder = TransformerEncoder(encoder_layer, n_layers, encoder_norm)
-        self.flatten = Flatten()
-        self.outlinear = Linear(d_model, num_classes)
-
-        """
-        self.sequential = Sequential(
-            ,
-            ,
-            ,
-            ,
-            ReLU(),
-
-        )
-        """
-
-    def forward(self,x):
-        x = self.inlinear(x)
-        x = self.relu(x)
-        x = x.transpose(0, 1) # N x T x D -> T x N x D
-        x = self.transformerencoder(x)
-        x = x.transpose(0, 1) # T x N x D -> N x T x D
-        x = x.max(1)[0]
-        x = self.relu(x)
-        x = self.outlinear(x)
-        # logits = self.outlinear(x)
-        # logprobabilities = F.log_softmax(logits, dim=-1)
-        # return logprobabilities
-        return x.squeeze(-1)
-
-class Flatten(nn.Module):
-    def forward(self, input):
-        return input.reshape(input.size(0), -1)
-
-
-                                                           
+                             
 
 
 
